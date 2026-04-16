deda41badf854322b6b5d1d30a445bde24a7fe02
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-16 13:59:22 -0300

    aula06b

---
+++ logicaProgramacao/06-TiposPrimitivosSaidaDados/aula06b.py
@@ -0,0 +1,10 @@
+n = float(input('Digite um valor: '))
+print(n)
+
+#Verdadeiro e falso se e um numero numerico.
+n = bool(input('Digite um valor: '))
+print(n)
+
+#Se é letra:
+n = input('Digite algo: ')
+print(n.isalpha())
