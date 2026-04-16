dc46bbc6a13ab0f60e276fe27bd0060c68734b3e
thauany.carvalho <thauany.carvalho-youxlab@institutoyoux.org.br>
2026-04-16 14:38:06 -0300

    desafio 3

---
+++ logica_python/05-iniciando_python/desafio 2.py
@@ -0,0 +1,4 @@
+dia = int(input('Qual o dia que vc nasceu?'))
+mes = input ('Que mes vc nasceu?')
+ano = input ('Que ano vc nasceu?')
+print (dia, mes, ano)
---
+++ logica_python/06-tipos_primitivo_saida_de_dados/desafio 3.py
@@ -0,0 +1,8 @@
+primeiroNumero = int(input('primeiro numero:'))
+segundoNumero= int(input('segundo numero:'))
+print(primeiroNumero + segundoNumero)
+
+
+
+
+
