f1df2d4dfbd5e07b6d5b5b2c5fddaef5f44f5292
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-20 14:07:03 -0300

    desafio007.py

--- logicaProgramacao/07-OperadoresAritmeticos/desafio007.py
+++ logicaProgramacao/07-OperadoresAritmeticos/desafio007.py
@@ -1,1 +1,6 @@
 #Desenvolva um programa que leia as duas notas de um aluno, calcule e mostra a sua media.
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+media = (n1 + n2) / 2
+print('A media entre {} e {} é igual a {}'.format(n1, n2, media))
