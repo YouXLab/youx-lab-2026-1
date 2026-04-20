6aaf669157bb7388def5a8cbfd6c8fe6f2d93a45
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-20 14:17:30 -0300

    desafio008.py

--- logicaProgramacao/07-OperadoresAritmeticos/desafio008.py
+++ logicaProgramacao/07-OperadoresAritmeticos/desafio008.py
@@ -1,1 +1,7 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+
+medida = float(input('Uma distancia em metros: '))
+cm = medida * 100
+mm = medida * 1000
+dm = medida * 10
+print('A medida de {}m corresponde a {}cm e {}mm'.format(medida, cm, mm, dm))
