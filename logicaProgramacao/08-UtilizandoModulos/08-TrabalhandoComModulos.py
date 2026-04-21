7e00f4d0a669eda734b4cf203aa1b2320df79a79
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-21 16:42:07 -0300

    08-TrabalhandoComNumeros.py

---
+++ logicaProgramacao/08-UtilizandoModulos/08-TrabalhandoComModulos.py
@@ -0,0 +1,20 @@
+#Import: Adicionar tudo.
+#from:Adicionar uma coisa.
+#math
+#ceil: Arredondar pra cima.
+#floor: Arredondar pra baixo
+#trunc: Eliminar o numero da virgula pra frente.
+#pow: Potencia.
+#sqrt: raiz.
+#factorial: fatoraçao
+                                # import math
+                                     #ou
+                                #from math import sqrt
+
+import math
+num = int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+
+
+
