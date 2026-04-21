946f504873c382e660035915f48efb81780795c4
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-21 19:43:16 -0300

    09-ManipulandoCadeiasDeTexto.py

---
+++ logicaProgramacao/09-ManipulandoTexto/09-ManipulandoCadeiasDeTexto.py
@@ -0,0 +1,35 @@
+frase = 'Curso em Video Python'
+print(frase[3])
+
+#Quantos O tem na frase.
+frase = 'Curso em Video Python'
+print(frase.count('o'))
+
+#tamanho da frase.
+frase = 'Curso em Video Python'
+print(len(frase))
+
+#Trocar alguma palavra.
+frase = 'Curso em Video Python'
+print(frase.replace('Python', 'Android'))
+
+#Salvar a frase.
+frase = 'Curso em Video Python'
+print(frase.replace('Python', 'Android'))
+print(frase)
+
+#Se alguma palavra esta dentro da frase.
+frase = 'Curso em Video Python'
+print('Curso' in frase)
+
+#Lista
+frase = 'Curso em Video Python'
+print(frase.split())
+
+#dividir
+frase = 'Curso em Video Python'
+dividido = frase.split()
+print(dividido[2][3])
+
+
+
