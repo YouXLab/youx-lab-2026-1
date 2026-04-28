7acbc68b8f22c45d6b511c4d439763496110ded3
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-04-28 13:54:38 -0300

    Exercicio 30

---
+++ logicaProgramacao/Ex030.py
@@ -0,0 +1,6 @@
+Numero = float(input('Digite um numero inteiro qualquer: '))
+Calculo = Numero % 2
+if Calculo == 0:
+    print(f'O numero {Numero} e par')
+else:
+    print(f'O numero {Numero} e impar')
