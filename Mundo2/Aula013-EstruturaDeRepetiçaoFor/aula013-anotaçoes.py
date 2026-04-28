94bf597e815d62af779e8311e6683da7421b1aef
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-28 17:55:37 -0300

    aula013

---
+++ Mundo2/Aula013-EstruturaDeRepetiçaoFor/aula013-anotaçoes.py
@@ -0,0 +1,21 @@
+#Laços de Repetiçao(parte1)
+
+#COMANDO PARA ANDAR EM LINHA RETA.
+# for c in range(1,10):
+    #passo
+#pega
+
+#COMANDO PARA PULAR ALGUNS.
+#for c in range(0,3)
+   #passo
+   #pula
+#passo
+#pega
+
+#COMANDO PARA PULAR ALGUNS E PEGAR MOEDAS.
+#for c in range(0,3)
+   #if moeda:   
+   #passo
+   #pula
+#passo
+#pega
