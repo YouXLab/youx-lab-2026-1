5dbd4447d3ce6bb2ea7bbd27ef31b00a779a4991
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-29 14:25:40 -0300

    aula013-anotaçoes

--- Mundo2/Aula013-EstruturaDeRepetiçaoFor/aula013-anotaçoes.py
+++ Mundo2/Aula013-EstruturaDeRepetiçaoFor/aula013-anotaçoes.py
@@ -19,3 +19,40 @@
    #pula
 #passo
 #pega
+
+#print('oi')
+#print('oi')
+#print('oi')
+
+#Contando de 1 a 6
+for c in range(1, 6):
+    print(c)
+print('FIM')
+
+#Intercalando em FIM e OI
+for c in range(1, 6):
+    print('oi')
+print('FIM')
+
+#subtraindo
+for c in range(1, 6, -0):
+    print(c)
+print('FIM')
+
+#pulando de 2 em 2
+for c in range(1, 7, 2):
+    print(c)
+print('FIM')
+#
+i = int(input('Inicio: '))
+f = int(input('FIM: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
+#Pedir pra voce digitar varios valores(somar os numeros).
+s = 0
+for c in range(1, 3):
+    n = int(input('Digite um valor:'))
+    s = s + n
+    print('O somatorio de todos os valores foi {}'.format(s)) 
