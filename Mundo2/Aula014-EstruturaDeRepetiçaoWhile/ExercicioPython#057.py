76d71ca6715078e1b6454df10606007cc1aba938
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-05-07 17:13:48 -0300

    exercicios

---
+++ Mundo2/Aula014-EstruturaDeRepetiçaoWhile/ExercicioPython#057.py
@@ -0,0 +1,9 @@
+# Exercício Python 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja
+# errado, peça a digitação novamente até ter um valor correto.
+
+sexo = str(input('Digite seu sexo: [F/M] ')).strip()[0]
+
+while sexo not in 'MmFf':
+    sexo = str(input('Digite seu sexo: [F/M] ')).strip()[0]
+
+print(f'Sexo {sexo} registrado com sucesso')
