54a6d15b8bcc837592d12fa32e5d77c58bca6dd8
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-05-12 13:58:13 -0300

    Exercicio 75

--- logicaProgramacao/Ex075.py
+++ logicaProgramacao/Ex075.py
@@ -1,2 +1,12 @@
 tupla = (int(input("Digite o primeiro número: ")), int(input("Digite o segundo número: ")), int(input("Digite o terceiro número: ")), int(input("Digite o quarto número: ")))
 print(tupla)
+nove = tupla.count(9)
+print (f"O numero nove aparece {nove} vezes ")
+if 3 in tupla:
+    print(f"O numero 3 esta na posicao {tupla.index(3)}")
+else:
+    print('O Valor 3 nao esta no programa!')
+print(f'Os numeros pares digitados foram ' ,end= '')
+for n in tupla:
+    if n % 2 == 0:
+        print(n,end= '')
