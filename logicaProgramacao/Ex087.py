a10b9838877f401625f84f2cfc06a9cd250a37c1
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-05-19 14:28:24 -0300

    Exercicio 87

---
+++ logicaProgramacao/Ex087.py
@@ -0,0 +1,21 @@
+matriz = [0,0,0],[0,0,0],[0,0,0]
+par = coluna = mai = 0
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para {l,c}: '))
+for l in range(0,3):
+    for c in range(0,3):
+        print(f"[{matriz[l][c]}]",end='')
+        if matriz[l][c] % 2 == 0:
+            par += matriz[l][c]
+    print()
+print(f"A soma dos pares e {par}")
+for l in range(0, 3):
+    coluna += matriz[l][2]
+print(f'A soma da terceira coluna e {coluna}')
+for c in range(0,3):
+    if c == 0:
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da segunda linha e {mai}')
