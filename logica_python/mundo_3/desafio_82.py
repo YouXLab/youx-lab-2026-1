ec7c99d5d89319c6341aa03036f5481b0c006178
allana santos <allana.silva-youxlab@institutoyoux.org.brgit config --global user.email allana.silva-youxlab@institutoyoux.org.br>
2026-05-23 11:27:47 -0300

    desafio_82

--- logica_python/mundo_3/desafio_82.py
+++ logica_python/mundo_3/desafio_82.py
@@ -2,3 +2,21 @@
 # Depois disso, crie duas listas extras que vão conter apenas os valores pares
 # e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 
+numeros = []
+pares = []
+impares = []
+
+continuar = 's'
+while continuar == 's ':
+    n = int (input('digite um valoe: '))
+    numeros.append(n)
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        impares.append(n)
+        continuar = input('quer continuar? [S/N ').upper()
+    print('-=' * 20)
+    print(f'lista completa: {numeros}')
+    print(f'lista dos pares: {pares}')
+    print(f'lista dos impares: {impares}')
+
