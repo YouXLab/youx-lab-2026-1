fa23684d86cb97d93122508f969fb0629160e574
allana santos <allana.silva-youxlab@institutoyoux.org.brgit config --global user.email allana.silva-youxlab@institutoyoux.org.br>
2026-05-23 11:08:51 -0300

    desafios_aula-17

--- logica_python/mundo_3/deafio_81.py
+++ logica_python/mundo_3/deafio_81.py
@@ -1,1 +1,20 @@
+# Exercício Python 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+# A) Quantos números foram digitados.
+# B) A lista de valores, ordenada de forma decrescente.
+# C) Se o valor 5 foi digitado e está ou não na lista
 
+r = 's'
+valores = []
+while r == 's':
+    valores.append(int(input('digite um valor: ')))
+    r = input('quer continuar? [S/N]').upper()
+
+print('-=' * 30)
+print(f'voce digitou {len(valores)} elementos. ')
+
+valores.sort(reverse=True)
+print(f'os valores em ordem decrescente sao {valores}')
+if 5 in valores:
+    print('o valor 5 faz parte da lista!')
+else:
+    print('o valor 5 nao foi encontrado na lista!')
