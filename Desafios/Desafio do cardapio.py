95c629f7df42dbb16c0bbe602aced5cd18c81d04
YouX <leonardo.alvarenga-youxlab@institutoyoux.org.br>
2026-05-27 14:25:17 -0300

    alfredo

---
+++ Desafios/Desafio do cardapio.py
@@ -0,0 +1,11 @@
+from time import sleep
+resposta = 'Ss'
+print('=+' * 20)
+print('---------------Bem Vindo----------------')
+print('+=' * 20)
+sleep (1)
+name = str(input('Qual e seu nome? '))
+sleep (1)
+while resposta in 'Ss':
+    str(input('Deseja ver o o cardapio? [S/N] :'))
+
---
+++ Desafios/Exercicio 108.py
@@ -0,0 +1,4 @@
+num = str(input('Digite o preço R$:'))
+for l in num:
+    resultado = num * 2
+print(f'O dobro {num} é {}')
